d6f36b16871d54c975de3e98cfa60767ec6b78b4
Andrey Sorokin <anderysorokin@gmail.com>
2017-09-15 13:46:56 +0300

    new solution
    implement method for large numbers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 module.exports = function multiply(first, second) {
-  
-}
-function(first,second){
-	return first*second;
+var tempArr=[],tempDigit=0;
+  for(var i=0; i<first.length;i++){
+    for(var j=0; j<second.length;j++){
+	  if(tempArr[i+j] === undefined){
+	    tempArr[i+j]=first[i]*second[j];  
+	  }else{
+	    tempArr[i+j]+=first[i]*second[j];	  
+	  }
+	} 
+  }
+  for(var i=tempArr.length-1;i>-1;i--){
+ 
+		tempArr[i]+=tempDigit;
+ 		tempDigit=(tempArr[i]-tempArr[i]%10)/10;
+    
+    tempArr[i]=tempArr[i]%10;
+ } 
+ if(tempDigit){tempArr.unshift(tempDigit)}
+ return tempArr.join("");
 }
